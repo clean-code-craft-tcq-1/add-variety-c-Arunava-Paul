35df934c130b88a252c73abda5c456f8da3aad06
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-03-31 02:12:48 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,66 +1,40 @@
-#include "typewise-alert.h"
-#include <stdio.h>
+#pragma once
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
-  }
-  if(value > upperLimit) {
-    return TOO_HIGH;
-  }
-  return NORMAL;
-}
+typedef enum {
+  PASSIVE_COOLING=0,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING,
+  MAX_COUNT_COOLING_TYPE
+} CoolingType;
 
-BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  int i,idx;
-  const CoolingType_TempLimit_st CtypeTL[MAX_COUNT_COOLING_TYPE]={{PASSIVE_COOLING,0,35},{HI_ACTIVE_COOLING,0,45},{MED_ACTIVE_COOLING,0,40}};
-  for(i = 0 ; i < MAX_COUNT_COOLING_TYPE ; i ++)
-  {
-	 if(coolingType == CtypeTL[i].coolingType ) 
-	 {
-		 idx = i;//Enum was possible to use in this line. But making this logic will avoid the stucture dependency on the enum order.
-	 }
-  }
-  return inferBreach(temperatureInC, CtypeTL[idx].temp_lowerLimit, CtypeTL[idx].temp_upperLimit);
-}
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+
+typedef enum {
+  TO_CONTROLLER,
+  TO_EMAIL
+} AlertTarget;
+
+typedef struct {
+  CoolingType coolingType;
+  char brand[48];
+} BatteryCharacter;
 
 void checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-  BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
-  );
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
 
-  switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
-  }
-}
-
-void sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
-
-void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
-  }
-}
+typedef struct 
+{
+	CoolingType coolingType;
+	double temp_lowerLimit;
+	double temp_upperLimit;
+}CoolingType_TempLimit_st;
