95dad50137a77f7e1a6b8a92c14a33d251118127
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-04-04 18:52:21 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -2,7 +2,54 @@
 
 #include "test/catch.hpp"
 #include "typewise-alert.h"
+#include "Test-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(1);//inferBreach(12, 20, 30) == TOO_LOW);
+/***Global variables*****/
+
+int Test_GUI = NO_OUTPUT;
+
+/************************/
+
+TEST_CASE("infers the breach according to limits") 
+{
+	int idx = 0;
+	Test_Parameters_st test_param[3]=
+	{
+		/*******baterry brand xxxx test ************/
+		{
+			TO_CONTROLLER,
+			{
+				PASSIVE_COOLING,
+				"xxxx",
+			},
+			52,
+			0x00
+		},
+		{
+			TO_CONTROLLER,
+			{
+				HI_ACTIVE_COOLING,
+				"xxxx",
+			},
+			52,
+			0x00
+		},
+		{
+			TO_EMAIL,
+			{
+				MED_ACTIVE_COOLING,
+				"xxxx",
+			},
+			52,
+			0x00
+		}
+	}
+	for(idx =0;idx<3;idx++)
+	{
+		checkAndAlert(test_param[idx].altr ,test_param[idx].bat_ch,test_param[idx].temp );
+		test_param[idx].GUI_RET = Test_GUI ; 
+		REQUIRE(test_param[idx].GUI_RET != NO_OUTPUT);
+		test_param[idx].GUI_RET = NO_OUTPUT ; // reset the value
+	}
+  
 }
