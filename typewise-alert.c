b4d867bb94e10275572288b29b2c4fd9a7d24a35
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-04-06 03:47:01 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -44,10 +44,10 @@ void checkAndAlert( AlertTarget alertTarget, BatteryCharacter batteryChar, doubl
 #endif
   /***************/
   BreachType breachType = classifyTemperatureBreach( batteryChar.batt_coolingType, temperatureInC );   
-  alertTarget_fcpt_a alertTarget_fcpt[MAX_NO_OF_TARGET] = {sendToController,sendToEmail,sendToConsole,empty_Func};
+  alertTarget_fcpt_a alertTarget_fcpt[4] = {sendToController,sendToEmail,sendToConsole,empty_Func}; /*[4] questionable content. Need to be checked*/
   if(alertTarget_fcpt[alertTarget] != 0) //Sanity check
   {
-	(*alertTarget_fcpt[0][1])(breachType);
+	(*alertTarget_fcpt[alertTarget])(breachType);
   }
   else
   {
