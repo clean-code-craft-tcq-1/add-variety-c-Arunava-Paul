39e80002266c6114f9b7aa1dd72673620855760d
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-04-07 01:04:10 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -88,7 +88,7 @@ void sendToEmail(BreachType breachType) {
 void sendToConsole(BreachType breachType)
 {
   const Console_info_st console_info[MAX_BREACH_TYPE] = {EMPTY_MSG,{"Hi, the temperature is too low"},{"Hi, the temperature is too high"}};
-  if(!strcmp(console_info[breachType].console_msg,EMPTY_MSG))
+  if(strcmp(console_info[breachType].console_msg,EMPTY_MSG))
   {
     printf("%s" , console_info[breachType].console_msg);
   }  
