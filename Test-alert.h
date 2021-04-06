08cdf6f83c7c10a2c29c7336163a0108b29af5a0
Arunava-Paul <77390499+Arunava-Paul@users.noreply.github.com>
2021-04-07 02:16:44 +0530

    Update Test-alert.h

--- Test-alert.h
+++ Test-alert.h
@@ -7,12 +7,14 @@
 #define EMAIL_OUTPUT		0xAA
 #define CONTROLLER_OUTPUT	0xBB
 #define CONSOLE_OUTPUT		0xCC
-#define NO_OUTPUT			0x00
+#define UNDEFINED_OUTPUT	0xDD
+#define NO_OUTPUT		0x00
 
 typedef struct
 {
 	AlertTarget altr;
 	BatteryCharacter bat_ch;
 	double temp;
-	int GUI_RET;
+	int GUI_RET_EXPECTED;
+	int GUI_RET_RECEIVED;
 }Test_Parameters_st;
